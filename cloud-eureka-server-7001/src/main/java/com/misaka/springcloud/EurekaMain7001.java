a4d93fd214e0dc0612166f46dbc537d027e117a2
misaka517 <136317512@qq.com>
2021-08-31 19:40:01 +0800

    eureka服务端，主启动类

---
+++ cloud-eureka-server-7001/src/main/java/com/misaka/springcloud/EurekaMain7001.java
@@ -0,0 +1,16 @@
+package com.misaka.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 517
+ * @date 2021-08-31 - 19:11
+ */
+
+@SpringBootApplication
+public class EurekaMain7001 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7001.class,args);
+    }
+}
